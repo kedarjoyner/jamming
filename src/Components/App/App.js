56b752d7e84888f9d844b55f2927a7afab425655
kedarjoyner <kedarjoyner@gmail.com>
2018-07-27 21:35:24 -0500

    Add fonts and reset to public, change background img, add and render SearchBar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import SearchBar from '../SearchBar/SearchBar';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App-wrap">
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            {/* Add a SearchBar component */}
+            <SearchBar />
             <div className="App-playlist">
               {/* Add a SearchResults component
               Add a Playlist component */}
