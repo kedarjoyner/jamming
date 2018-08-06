e403111ddaf4ab0738d02fc842f7a46f0958ea0f
kedarjoyner <kedarjoyner@gmail.com>
2018-08-05 19:36:10 -0500

    Retrieve access token, expiration, clear url paramas

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,32 +1,52 @@
-const accessToken = '';
-const authorizeUrl = 'https://accounts.spotify.com/authorize/?';
+let accessToken = '';
+let authorizeUrl = 'https://accounts.spotify.com/authorize?';
 const clientId = 'client_id=04772450bf1842468d319210630d7c46';
 const responseType = '&response_type=token';
-const redirectUri = '&redirect_uri=http://localhost:3000/callback/';
-const scope = '&scope=playlist-modify-public'
+const redirectUri = '&redirect_uri=http://localhost:3000/callback';
+const scope = '&scope=playlist-modify-public';
 
 
 let Spotify = {
 
-    getAccessToken(){
-      
-     const endpoint = `${authorizeUrl}${clientId}${redirectUri}${scope}${responseType}`;
+    getAccessToken(){      
+        authorizeUrl += `${clientId}${redirectUri}${scope}${responseType}`;
+     
+     if( accessToken ) {
+        return accessToken;
 
-     fetch(endpoint).then(response => {
-         if(response.ok) {
-             return response;
-         }
-         throw new Error('Request failed!');
+     } else if( accessToken !== undefined && accessToken !== null ) {
+        const url = window.location.href;
+        accessToken = url.match(/access_token=([^&]*)/);
+        let expiresIn = url.match(/expires_in=([^&]*)/);
 
-        }, networkError => {
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
 
-        console.log(networkError.message)
+     } else {
+        window.location = authorizeUrl;
 
-        }).then(response => {
-            // renderResponse(jsonResponse);
-            console.log(response);
-        });
-    }   
+     }
+
+    //  fetch(endpoint, {
+    //     method: "GET", // *GET, POST, PUT, DELETE, etc.
+    //     headers: {
+    //         'Authorization': 'Bearer ' + accessToken,
+    //     }
+    //  }).then(response => {
+    //      if(response.ok) {
+    //          return response;
+    //      }
+    //      throw new Error('Request failed!');
+
+    //     }, networkError => {
+
+    //     console.log(networkError.message)
+
+    //     }).then(response => {
+    //         console.log(response);
+    //     });
+    // } 
+    }  
 }
 
 
